99140566c423f17ccaa175e0f0feb5f86bc99080
isaacmejia <isaac.mejia.est@tecazuay.edu.ec>
2023-01-18 19:57:31 -0500

    miguel

--- src/main/java/MejiaTendencias/demospringboots3/Demospringboots3Application.java
+++ src/main/java/MejiaTendencias/demospringboots3/Demospringboots3Application.java
@@ -3,11 +3,18 @@ package MejiaTendencias.demospringboots3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Demospringboots3Application {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demospringboots3Application.class, args);
+
 	}
+
 
 }
--- src/main/java/MejiaTendencias/demospringboots3/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package MejiaTendencias.demospringboots3.repository;
-
-import MejiaTendencias.demospringboots3.model.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
-}
